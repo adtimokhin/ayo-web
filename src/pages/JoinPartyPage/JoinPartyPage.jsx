23527293f9144f63d7fb45e51a51788ee84fa381
adtimokhin <adtimokhin@gmail.com>
2023-04-25 20:21:46 -0500

    Managed to break the system for redirecting people after scannning party QR. But at least now camera stops making video after the QR was scanner

--- src/pages/JoinPartyPage/JoinPartyPage.jsx
+++ src/pages/JoinPartyPage/JoinPartyPage.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import jsQR from "jsqr";
 import { useNavigate } from "react-router";
 import { getCurrentUser } from "../../util/auth";
@@ -12,8 +12,26 @@ import {
 import { homeDirectory } from "../../util/routing";
 
 function JoinPartyPage() {
+  // TODO: DOES NOT WORK. REDO THIS COMPONENT!
   const videoRef = useRef();
   const navigate = useNavigate();
+
+  const [stream, setStream] = useState(null);
+
+  useEffect(() => {
+    const constraints = {
+      video: true,
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((stream) => {
+        setStream(stream);
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices.", error);
+      });
+  }, []);
 
   let user;
 
@@ -26,15 +44,14 @@ function JoinPartyPage() {
       // Redirect to login page if user is not signed in
       navigate(`${homeDirectory}/login`);
     } else {
-
       const constraints = {
         audio: false,
         video:
-        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-          navigator.userAgent
-        )
-          ? { facingMode: { exact: "environment" } }
-          : true,
+          /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+          )
+            ? { facingMode: { exact: "environment" } }
+            : true,
       };
 
       const canvas = document.createElement("canvas");
@@ -91,12 +108,30 @@ function JoinPartyPage() {
               });
             }
           }, 1000);
-          return () => clearInterval(interval);
+          return () => {
+            clearInterval(interval);
+            if (stream) {
+              stream.getTracks().forEach((track) => {
+                track.stop();
+                navigate(`${homeDirectory}/home`);
+              });
+            }
+            navigate(`${homeDirectory}/home`);
+          };
         })
         .catch((err) => {
           console.log(err);
         });
     }
+
+    return () => {
+      console.log("Function is called");
+      if (stream) {
+        stream.getTracks().forEach((track) => {
+          track.stop();
+        });
+      }
+    };
   }, []);
 
   // TODO: FORCE DELETE THE LISTENER WHEN THE COMPONENT LEAVES THE SCREEN
